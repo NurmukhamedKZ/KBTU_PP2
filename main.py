11e20810a0d6b872601822f3c844dd4c6c2c9c4b
Nurmukhamed mac <nurekeashekei@gmail.com>
2026-01-20 14:58:12 +0500

    solved 1,2,3

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def main():
-    name = input()
-    print(f"Hello, {name}!")
+    a = input()
+    # try:
+    #     int(a)
+    #     print("int")
+    # except Exception as e:
+    #     print("str")
+
+    if a.isdigit():
+        print(
+            "int"
+        )
+    else:
+        print("str")
 
 
 if __name__ == "__main__":
