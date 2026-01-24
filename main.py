2b6bf2400bee45caa6c6c056135012b6f7b36def
Nurmukhamed mac <nurekeashekei@gmail.com>
2026-01-24 23:09:01 +0500

    printed hello worlds withoun new line

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-def main():
-    a = int(input())
-    b = int(input())
-    if a == b:
-        print("equal")
-    else:
-        print(max(a,b))
-    
-    
-    
-if __name__ == "__main__":
-    main()
+print("Hello World!", end=" ")
