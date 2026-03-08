21fd99b5fc8effbad959ca53ab56955961ea1db9
Nurmukhamed mac <nurekeashekei@gmail.com>
2026-03-08 10:36:46 +0500

    feat(ex9): insert spaces between words starting with capital letters

---
+++ Practice5/Regex_exercises/ex_9.py
@@ -0,0 +1,14 @@
+"""
+9. Write a Python program to insert spaces between words starting with capital letters.
+"""
+import re
+
+
+def insert_spaces_before_capitals(text: str) -> str:
+    """Insert spaces between words starting with capital letters."""
+    return re.sub(r"([a-z])([A-Z])", r"\1 \2", text)
+
+
+if __name__ == "__main__":
+    text = "HelloWorldPython"
+    print(insert_spaces_before_capitals(text))  # Hello World Python
